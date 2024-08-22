c436c99ad0f1c1e6ac78721eaa29984f61731ff5
Adi Naik <161313416+3011adi@users.noreply.github.com>
2024-08-22 22:38:11 +0530

    b

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -3,10 +3,20 @@ import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
   // Explicitly typing the position as a tuple of two numbers (latitude and longitude)
-  const position: [number, number] = [15.5933, 73.8085]; // Replace with your exact location
+  const position: [number, number] = [49.2125578, 16.62662018]; // Replace with your exact location
 
   return (
-    <div>map</div>
+    <MapContainer center={position} zoom={13} style={{ height: "60vh", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
