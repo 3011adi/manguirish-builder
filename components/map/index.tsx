bb4130fe007e581790d9f8b1af3c61735af3f058
Adi Naik <161313416+3011adi@users.noreply.github.com>
2024-08-22 19:53:14 +0530

    map

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -1,13 +1,22 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-
 const Map = () => {
   // Explicitly typing the position as a tuple of two numbers (latitude and longitude)
   const position: [number, number] = [15.5933, 73.8085]; // Replace with your exact location
 
   return (
-   <div>map</div>
+    <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
