ce6819cc9fc4972369baed4e5510e3b18a76e1c3
Adi Naik <161313416+3011adi@users.noreply.github.com>
2024-08-22 23:01:34 +0530

    b

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -1,17 +1,17 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
+const pos: [number, number] = [49.2125578, 16.62662018];
 const Map = () => {
   // Explicitly typing the position as a tuple of two numbers (latitude and longitude)
-  const position: [number, number] = [49.2125578, 16.62662018]; // Replace with your exact location
+   // Replace with your exact location
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: "60vh", width: "100%" }}>
+    <MapContainer center={pos} zoom={13} style={{ height: "60vh", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
+      <Marker position={pos}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
